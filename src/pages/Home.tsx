3fac7c7fb9e9a1550a3943d053276063fd70e76e
Mahdi Allam <mahdi@Mahdis-MacBook-Pro.local>
2024-02-26 13:01:48 +0100

    test

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,7 +8,6 @@ const Home = () => {
     <div className="home">
       <div className="home-container">
         <Header />
-        <Search />
         <Gallery />
         <Footer />
       </div>
